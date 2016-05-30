39f302f6fba4fa1daa6e24b5fd08721bdcd61675
Bohdan Shtepan <winsatana@ya.ru>
2016-05-30 04:23:39 +0300

    Adds styles compiling script

---
+++ compile-styles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Make sure you have Node.js and node-sass package installed.
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+if exists node && exists node-sass; then
+	node --version
+	node-sass --v
+else 
+	echo $'Node.js or node-sass package is not installed.\nMake sure you have installed all the required software.'
+	
+	exit
+fi
+
+node-sass --output-style 'nested' src/paper-ui.scss examples/paper-ui.css
+
+exit
